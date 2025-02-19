797722634d8322e03425e779ffe8dc0e52c33cd9
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2025-02-19 22:39:55 +0100

    feat: add cors to api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,8 @@
 import { Elysia } from "elysia";
 import { pocketsRoute } from "./routes/pockets";
+import cors from "@elysiajs/cors";
 
-const app = new Elysia().use(pocketsRoute).listen(3000);
+const app = new Elysia().use(cors()).use(pocketsRoute).listen(3000);
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
