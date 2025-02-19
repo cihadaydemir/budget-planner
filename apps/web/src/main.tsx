6ff43f7276929b2a5b51b4922d134a2926316fd0
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2025-02-19 23:50:33 +0100

    feat: configure tanstack query provider

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,10 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import ReactDOM from "react-dom/client";
 import { routeTree } from "./routeTree.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 // Set up a Router instance
 const router = createRouter({
@@ -19,5 +23,10 @@ const rootElement = document.getElementById("app")!;
 
 if (!rootElement.innerHTML) {
 	const root = ReactDOM.createRoot(rootElement);
-	root.render(<RouterProvider router={router} />);
+	root.render(
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>,
+	);
 }
