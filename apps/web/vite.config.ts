fa5150e8c1e1d655d602f5cf196ace8b709be68d
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2025-03-06 00:31:36 +0100

    feat: add vite pwa + init configuration, add pwa assets

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,10 +3,35 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [tailwindcss(), TanStackRouterVite({}), react()],
+	plugins: [
+		VitePWA({
+			registerType: "autoUpdate",
+			devOptions: {
+				enabled: true,
+			},
+			strategies: "generateSW",
+			// srcDir: "./src",
+			//  filename: "service-worker.ts",
+			// injectManifest: {
+			// 	swDest: "dist/service-worker.js",
+			// },
+			manifest: {
+				name: "Budget Planner",
+				short_name: "Budget Planner",
+				theme_color: "#020618",
+				background_color: "#020618",
+				display: "standalone",
+				start_url: "/",
+			},
+		}),
+		tailwindcss(),
+		TanStackRouterVite({}),
+		react(),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
