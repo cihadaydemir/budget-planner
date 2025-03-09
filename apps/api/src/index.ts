c066a548f016ef1505e1291794cd364961d3e2e6
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2025-03-09 01:55:05 +0100

    feat: add d1 db env

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,21 @@
-import type { Env } from "bun";
 import type { Context } from "elysia";
 
 import { app } from "./app";
 
+declare global {
+	var env: Env;
+}
+
 export default {
 	async fetch(request: Request, env: Env, ctx: Context): Promise<Response> {
+		Object.assign(globalThis, {
+			env,
+		});
+
+		Object.assign(process, {
+			env: { ...env, DATABASE_URL: env.devDB },
+		});
+
 		return await app.fetch(request);
 	},
 } satisfies ExportedHandler<Env>;
