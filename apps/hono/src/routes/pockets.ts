ebef001a9cc18c984a68429e1ce9edfc1107c392
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2025-03-18 00:45:00 +0100

    feat: get userId from session

--- apps/hono/src/routes/pockets.ts
+++ apps/hono/src/routes/pockets.ts
@@ -1,13 +1,19 @@
-import { AppContext } from "..";
-import { Hono } from "hono";
-import { eq } from "drizzle-orm";
-import { pocket } from "../db/schema";
+import type { AppContext } from ".."
+import { Hono } from "hono"
+import { eq } from "drizzle-orm"
+import { pocket } from "../db/schema"
 
-const app = new Hono<AppContext>();
-
+const app = new Hono<AppContext>()
+//TODO: add session to the context before the route
 app.get("/", async (c) => {
-	const db = c.var.DrizzleDB;
-	return c.json(await db?.select().from(pocket).where(eq(pocket.userId, c.var.session.user.id)));
-});
+	const db = c.var.DrizzleDB
+	const session = await c.var.auth.api.getSession({ headers: c.req.raw.headers })
+	if (!session) {
+		console.error("no session", session)
+		return c.json({ error: "no session" })
+	}
 
-export const pocketsRoute = app;
+	return c.json(await db?.select().from(pocket).where(eq(pocket.userId, session.user.id)))
+})
+
+export const pocketsRoute = app
