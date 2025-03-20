f79346039399658124c350b41d0ed7093bd12def
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2025-03-20 21:06:25 +0100

    feat: add credentials to hono clients header

--- apps/web/src/lib/hono-client.ts
+++ apps/web/src/lib/hono-client.ts
@@ -1,4 +1,8 @@
 import type { AppType } from "../../../hono/src/index"
 import { hc } from "hono/client"
 
-export const hono = hc<AppType>(import.meta.env.VITE_BETTER_AUTH_URL as string)
+export const hono = hc<AppType>(import.meta.env.VITE_BETTER_AUTH_URL as string, {
+	init: {
+		credentials: "include",
+	},
+})
