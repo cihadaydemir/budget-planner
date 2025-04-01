7b3fd72328869a85510159b2ffbd4c80743b9253
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2025-04-01 20:34:16 +0200

    refactor: remove unnecessary fragment and adjust styling

--- apps/web/src/components/pockets-card.tsx
+++ apps/web/src/components/pockets-card.tsx
@@ -20,68 +20,65 @@ export const PocketsCard = ({ pocket, isOpen, setIsOpen, setEditingPocket }: Poc
 	const deletePocketMutation = useDeletePocket()
 
 	return (
-		<>
-			<Card
-				key={pocket.id}
-				onClick={() =>
-					navigate({
-						to: "/pocket/$pocketId",
-						params: { pocketId: pocket.id },
-					})
-				}
-				className="w-full min-w-[250px] cursor-pointer"
-			>
-				<Card.Header className=" flex w-full flex-row items-center justify-between">
-					<Card.Title>
-						<p className="font-bold">{pocket.name}</p>
-					</Card.Title>
-					<Menu>
-						<Button size="square-petite" intent="plain">
-							<IconDotsVertical className="size-4" />
-						</Button>
+		<Card
+			key={pocket.id}
+			onClick={() =>
+				navigate({
+					to: "/pocket/$pocketId",
+					params: { pocketId: pocket.id },
+				})
+			}
+			className="w-full min-w-[250px] cursor-pointer"
+		>
+			<Card.Header className=" flex w-full flex-row items-center justify-between">
+				<Card.Title>
+					<p className="font-bold">{pocket.name}</p>
+				</Card.Title>
+				<Menu>
+					<Button size="square-petite" intent="plain">
+						<IconDotsVertical className="size-4" />
+					</Button>
 
-						<Menu.Content>
-							<Menu.Item
-								onAction={() => {
-									setEditingPocket(pocket)
-									setIsOpen(true)
-								}}
-							>
-								<IconHighlight />
-								Edit
-							</Menu.Item>
-							<Menu.Separator />
-							<Menu.Item
-								isDanger
-								onAction={() =>
-									deletePocketMutation.mutate(pocket.id, {
-										onSuccess: () => {
-											toast.success(`Pocket ${pocket.name} deleted successfully`)
-											queryClient.invalidateQueries({
-												queryKey: ["pockets"],
-											})
-										},
-									})
-								}
-							>
-								<IconTrash />
-								Delete
-							</Menu.Item>
-						</Menu.Content>
-					</Menu>
-				</Card.Header>
-				<Card.Content className="flex flex-col gap-2">
-					<p>{pocket.description}</p>
-					{pocket.budget && <p>{pocket.budget.toLocaleString("DE-de")} EUR</p>}
-					<Meter
-						label="Spent"
-						value={pocket.totalSpent}
-						maxValue={pocket.budget ?? undefined}
-						formatOptions={{ style: "currency", currency: "EUR" }}
-						// valueLabel={getValueLabelText()}
-					/>
-				</Card.Content>
-			</Card>
-		</>
+					<Menu.Content>
+						<Menu.Item
+							onAction={() => {
+								setEditingPocket(pocket)
+								setIsOpen(true)
+							}}
+						>
+							<IconHighlight />
+							Edit
+						</Menu.Item>
+						<Menu.Separator />
+						<Menu.Item
+							isDanger
+							onAction={() =>
+								deletePocketMutation.mutate(pocket.id, {
+									onSuccess: () => {
+										toast.success(`Pocket ${pocket.name} deleted successfully`)
+										queryClient.invalidateQueries({
+											queryKey: ["pockets"],
+										})
+									},
+								})
+							}
+						>
+							<IconTrash />
+							Delete
+						</Menu.Item>
+					</Menu.Content>
+				</Menu>
+			</Card.Header>
+			<Card.Content className="flex w-full flex-col gap-2">
+				<p>{pocket.description}</p>
+				{pocket.budget && <p>{pocket.budget.toLocaleString("DE-de")} EUR</p>}
+				<Meter
+					label="Spent"
+					value={pocket.totalSpent}
+					maxValue={pocket.budget ?? undefined}
+					formatOptions={{ style: "currency", currency: "EUR" }}
+				/>
+			</Card.Content>
+		</Card>
 	)
 }
--- apps/web/src/routes/_app.tsx
+++ apps/web/src/routes/_app.tsx
@@ -1,22 +1,22 @@
-import { Container, SidebarInset, SidebarProvider } from "@/components/ui";
-import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
+import { Container, SidebarInset, SidebarProvider } from "@/components/ui"
+import { Outlet, createFileRoute, redirect } from "@tanstack/react-router"
 
-import AppSidebar from "@/components/sidebar/app-sidebar";
-import AppSidebarNav from "@/components/sidebar/app-sidebar-nav";
-import { authQueryOptions } from "@/lib/auth/use-auth";
+import AppSidebar from "@/components/sidebar/app-sidebar"
+import AppSidebarNav from "@/components/sidebar/app-sidebar-nav"
+import { authQueryOptions } from "@/lib/auth/use-auth"
 
 export const Route = createFileRoute("/_app")({
 	component: AppLayout,
 	beforeLoad: async ({ context }) => {
-		const authData = await context.queryClient.ensureQueryData(authQueryOptions);
+		const authData = await context.queryClient.ensureQueryData(authQueryOptions)
 
 		if (!authData) {
 			throw redirect({
 				to: "/auth/sign-in",
-			});
+			})
 		}
 	},
-});
+})
 
 function AppLayout() {
 	return (
@@ -24,10 +24,10 @@ function AppLayout() {
 			<AppSidebar collapsible="dock" />
 			<SidebarInset>
 				<AppSidebarNav />
-				<Container intent="padded-content" className="h-full w-full">
+				<Container intent="padded-content" className="h-full w-full overflow-hidden">
 					<Outlet />
 				</Container>
 			</SidebarInset>
 		</SidebarProvider>
-	);
+	)
 }
