90f21a3e1b201ee4478c37f4a6d3336d63e3f839
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2025-04-06 23:18:05 +0200

    chore: remove log

--- apps/web/src/hooks/pockets/usePockets.ts
+++ apps/web/src/hooks/pockets/usePockets.ts
@@ -6,7 +6,7 @@ export const usePockets = () =>
 		queryKey: ["pockets"],
 		queryFn: async () => {
 			const res = await hono.api.pockets.$get()
-			console.log("pocket", res)
+
 			if (!res.ok) {
 				throw new Error("Failed to fetch pockets")
 			}
